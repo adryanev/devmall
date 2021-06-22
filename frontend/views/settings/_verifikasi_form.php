e4915c117c3ebedcba33fdec288fe063398d4321
adryanev <adryanekavandra@gmail.com>
2021-06-22 15:30:29 +0700

    update verifikasi form

--- frontend/views/settings/_verifikasi_form.php
+++ frontend/views/settings/_verifikasi_form.php
@@ -11,34 +11,35 @@ use yii\widgets\ActiveForm;
 /* @var $model VerifikasiForm */
 /* @var $form ActiveForm */
 /* @var $verifikasiSekarang VerifikasiUser */
-$not_verified = isset($verifikasiSekarang)? $verifikasiSekarang->status === VerifikasiUser::STATUS_DITOLAK: null;
+$not_verified = isset($verifikasiSekarang) ? $verifikasiSekarang->status === VerifikasiUser::STATUS_DITOLAK : null;
 ?>
 
 <div class="row">
     <div class="col-lg-12">
         <?php if (isset($not_verified)): ?>
-        <h5 class="text-capitalize">Status Verifikasi Terakhir Ini</h5>
-        <div class="table-responsive">
-            <table class="table withdraw__table">
-                <thead>
-                <tr>
-                    <th>Berkas</th>
-                    <th>Status</th>
-                </tr>
-                </thead>
+            <h5 class="text-capitalize">Status Verifikasi Terakhir Ini</h5>
+            <div class="table-responsive">
+                <table class="table withdraw__table">
+                    <thead>
+                    <tr>
+                        <th>Berkas</th>
+                        <th>Status</th>
+                    </tr>
+                    </thead>
 
-                <tbody>
-                <tr>
-                    <td><?= Html::img('@web/upload/verifikasi/' . $verifikasiSekarang->nama_file, ['width'=>'50%']) ?></td>
-                    <td class="bold"><?=$verifikasiSekarang->getStatusVerifikasi()?></td>
-                </tr>
-                </tbody>
-            </table>
-    </div>
-            <?php
+                    <tbody>
+                    <tr>
+                        <td><?= Html::img('@web/upload/verifikasi/' . $verifikasiSekarang->nama_file,
+                                ['width' => '50%']) ?></td>
+                        <td class="bold"><?= $verifikasiSekarang->getStatusVerifikasi() ?></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        <?php
         endif;
         ?>
-</div>
+    </div>
     <?php
     if ($not_verified || !isset($verifikasiSekarang)):
         ?>
@@ -71,4 +72,5 @@ $not_verified = isset($verifikasiSekarang)? $verifikasiSekarang->status === Veri
             <?php ActiveForm::end(); ?>
 
         </div><!-- _ganti_password_form -->
-    <?php endif;?>
+    <?php endif; ?>
+</div>
